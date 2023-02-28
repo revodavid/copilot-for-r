42f9e60ff2215f39567888e9f5bbc165fe1ff8c4
David Smith <david@nonfamous.com>
2023-02-28 14:47:54 -0800

    Updated pumpkins demo

--- pumpkins.R
+++ pumpkins.R
@@ -1,5 +1,3 @@
-R.version
-
 library(tidyverse)
 
 # read in pumpkin data
@@ -16,16 +14,24 @@ glimpse(pumpkins)
 # display 5 random rows
 pumpkins %>% sample_n(5)
 
+# show average high price by package and color
+pumpkins %>% group_by(package) %>% summarise(avg_high_price = mean(high_price))
+
+# analysis of variance of high price by origin, color and item_size
+model <- aov(high_price ~ origin + color + item_size, data = pumpkins)
+
+# display AOV table
+summary(model)
+
+# predict ppb for a pumpkin with the following characteristics
+predict(model, data.frame(origin = "OHIO", color = "ORANGE", item_size = "med"))
+
+# DEMO STOPS HERE. But a few other examples are below.
+
 # filter rows where "bushel" is found in the package name
 pumpkins %>% filter(str_detect(package, "bushel")) -> pumpkins
 
-# create a table of counts by item_size and color
-pumpkins %>% count(item_size, color)
-
-# show average high price by package
-pumpkins %>% group_by(package) %>% summarise(avg_high_price = mean(high_price))
-
-# calculate high price per bushel as column ppb using package column
+# calculate price per bushel as ppb using package name
 pumpkins %>% mutate(ppb = high_price / case_when(
     str_detect(package, "1/2") ~ 0.5,
     str_detect(package, "1/19") ~ 0.25,
@@ -34,11 +40,6 @@ pumpkins %>% mutate(ppb = high_price / case_when(
 # table of average ppb by color and item_size
 pumpkins %>% group_by(color, item_size) %>% summarise(avg_ppb = mean(ppb))
 
-# analysis of variance of ppb by origin, color and item_size
-model <- aov(ppb ~ origin + color + item_size, data = pumpkins)
+# create a table of counts by item_size and color
+pumpkins %>% count(item_size, color)
 
-# display AOV table
-summary(model)
-
-# predict ppb for a pumpkin with the following characteristics
-predict(model, data.frame(origin = "OHIO", color = "ORANGE", item_size = "med"))
